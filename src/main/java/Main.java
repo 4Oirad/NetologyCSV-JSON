a611c3a9747b45a6778afaba59c00c7f18573ee6
Bair Arshaev <ars-bair@yandex.ru>
2022-05-16 23:00:06 +0300

    edit code 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,20 +5,32 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json, "data.json");
+
+        List<Employee> list1 = parseXML("data.xml");
+        String json1 = listToJson(list1);
+        writeString(json1, "data2.json");
     }
 
     static void writeString(String json, String jsonFileName) {
@@ -54,5 +66,50 @@ public class Main {
             e.printStackTrace();
         }
         return null;
+    }
+
+    static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        long id = 0;
+        String firstName = null;
+        String lastName = null;
+        String country = null;
+        int age = 0;
+        List<Employee> list = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory. newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse( new File(fileName));
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node1 = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node1.getNodeType()) {
+                NodeList nodeList1 = node1.getChildNodes();
+                for (int j = 0; j < nodeList1.getLength(); j++) {
+                    Node node2 = nodeList1.item(j);
+                    if (Node.ELEMENT_NODE == node2.getNodeType()) {
+                        Element element = (Element) node2;
+                        switch (element.getNodeName()) {
+                            case "id":
+                                id = Long.parseLong(element.getTextContent());
+                                break;
+                            case "firstName":
+                                firstName = element.getTextContent();
+                                break;
+                            case "lastName":
+                                lastName= element.getTextContent();
+                                break;
+                            case "country":
+                                country= element.getTextContent();
+                                break;
+                            case "age":
+                                age= Integer.parseInt(element.getTextContent());
+                                break;
+                        }
+                    }
+                }
+                list.add(new Employee(id, firstName, lastName, country, age));
+            }
+        }
+        return list;
     }
 }
