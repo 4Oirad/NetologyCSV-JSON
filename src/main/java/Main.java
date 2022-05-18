ed3027a017cbc44e0933eb3b35ebe6f3b947aa2a
Bair Arshaev <ars-bair@yandex.ru>
2022-05-18 23:32:19 +0300

    edit code 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,32 +5,60 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+
+        //Task1
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json, "data.json");
 
+        //Task2
         List<Employee> list1 = parseXML("data.xml");
         String json1 = listToJson(list1);
         writeString(json1, "data2.json");
+
+        //Task3
+        File file = new File("new_data.json");
+        List<Employee> list2 = jsonToList(file);
+        for (Employee employee: list2) {
+            System.out.println(employee);
+        }
+    }
+
+    static List<Employee> jsonToList(File jsonFile) {
+
+        JSONParser parser = new JSONParser();
+        List<Employee> list = new ArrayList<>();
+        try {
+            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(jsonFile));
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            for (Object object : jsonArray) {
+                list.add(gson.fromJson(String.valueOf(object), Employee.class));
+            }
+            return list;
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     static void writeString(String json, String jsonFileName) {
@@ -47,8 +75,7 @@ public class Main {
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.setPrettyPrinting();
         Gson gson = gsonBuilder.create();
-        String json = gson.toJson(list, listType);
-        return json;
+        return gson.toJson(list, listType);
     }
 
     static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -60,8 +87,7 @@ public class Main {
             CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
                     .withMappingStrategy(strategy)
                     .build();
-            List<Employee> staff = csv.parse();
-            return staff;
+            return csv.parse();
         } catch (IOException e) {
             e.printStackTrace();
         }
